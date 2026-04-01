b72de31d0fe40750be08dd3ec9c4113959583e49
cgstever <65565649+cgstever@users.noreply.github.com>
2026-04-01 14:25:13 -0400

    Update Demetri_Repertoire_Module.js
    update version

--- Planing/Demetri_Repertoire_Module.js
+++ Planing/Demetri_Repertoire_Module.js
@@ -40,7 +40,7 @@ function detectPendingForm(messages, knownForms) {
 
 export default {
     name: 'Demetri Repertoire',
-    version: '2.0.0',
+    version: '2.1.0',
 
     init(data) {
         return {
